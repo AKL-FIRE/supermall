0a62fd5c5adcc0cc7fa0b3e73bff0e6cd39bad38
lcy <lcy19961018@163.com>
2020-05-18 17:54:12 -0700

    03.add home's banner to the project

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,46 @@
 <template>
-  <div>
-    <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template #center>
+        <h2>购物街</h2>
+      </template>
+    </nav-bar>
+    <my-swiper :banners="banners"></my-swiper>
   </div>
 </template>
 
 <script>
+
+  import NavBar from "../../components/common/navbar/NavBar";
+  import MySwiper from "./childComps/MySwiper";
+  import {getHomeMultidata} from "../../network/home";
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar,
+      MySwiper
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+      }
+    },
+    created() {
+      // 1. 请求数据
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
   }
 </script>
 
 <style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 
 </style>
---
+++ src/views/home/childComps/MySwiper.vue
@@ -0,0 +1,64 @@
+<template>
+  <swiper ref="mySwiper" :options="swiperOptions" class="my-swiper">
+    <swiper-slide v-for="(item, index) in banners" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" :alt="item.title">
+      </a>
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+
+  import {Swiper, SwiperSlide, directive} from 'vue-awesome-swiper'
+
+  export default {
+    name: "MySwiper",
+    props: {
+      banners: Object
+    },
+    components: {
+      Swiper,
+      SwiperSlide
+    },
+    directives: {
+      swiper: directive
+    },
+    data() {
+      return {
+        swiperOptions: {
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          // Some Swiper option/callback...
+          // 循环
+          loop: true,
+          // 自动播放
+          autoplay: {
+            delay: 1500,
+            stopOnLastSlide: false,
+            disableOnInteraction: false
+          }
+        }
+      }
+    },
+    computed: {
+      swiper() {
+        return this.$refs.mySwiper.$swiper
+      }
+    },
+    mounted() {
+      console.log('Current Swiper instance object', this.swiper)
+      this.swiper.slideTo(3, 1000, false)
+    }
+  }
+</script>
+
+<style scoped>
+  @import "../../../../node_modules/swiper/css/swiper.css";
+
+  .my-swiper img {
+    width: 100%;
+  }
+</style>
